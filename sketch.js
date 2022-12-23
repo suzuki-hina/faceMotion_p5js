0aa7d915e7acda2f3b289552e7062c3975bcf3b0
suzuki-hina <ichigodaifuku.0530@gmail.com>
2022-12-23 20:48:49 +0900

    レコーディングチャレンジ

--- sketch.js
+++ sketch.js
@@ -45,6 +45,11 @@ let TdecisionButton;
 let deformButton;
 let DdecisionButton;
 let animationButton;
+let recordingButton;
+
+//動画のレコーディング
+let recordingChanger = false;
+let isRecording = false;
 
 
 function setup() {
@@ -52,6 +57,7 @@ function setup() {
   window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
   window.addEventListener("touchmove", function (event) { event.preventDefault(); }, { passive: false });
 
+  //キャンバスの設定
   createCanvas(windowWidth, windowHeight, WEBGL);
   img = loadImage("img/parkFace.jpg");
 
@@ -63,8 +69,7 @@ function setup() {
 
   //ポイントの位置とテクスチャの配列設定
   //パーツを囲う図形の線上の点の配列設定
-  //初期フレームの配列設定
-  //最終フレームの配列設定
+  //初期と最終フレームの配列設定
   let pPx = -width / 2;
   let pTx = -width / 2;
   let pOLx = -width / 2;
@@ -200,6 +205,11 @@ function setup() {
   animationButton.position(w * 6, w * 9);
   animationButton.size(w, w);
   animationButton.mousePressed(AnimationButton);
+
+  recordingButton = createButton("recording");
+  recordingButton.position(w * 7, w * 9);
+  recordingButton.size(w, w);
+  recordingButton.mousePressed(RecordingButton);
 }
 
 function draw() {
@@ -240,7 +250,7 @@ function draw() {
       for (let j = 0; j < rows; j++) {
         if (mouseIsPressed == true) {
           let d = dist(pP[i][j].x, pP[i][j].y, mouseX - width / 2, mouseY - height / 2);
-          if (d < 10) {
+          if (d < w / 4) {
             pP[i][j].x = mouseX - width / 2;
             pP[i][j].y = mouseY - height / 2;
           }
@@ -374,96 +384,64 @@ function draw() {
             if ((mark[k].my - w / 2 < pT[i][j].y && pT[i][j].y < mark[k + 1].my + w / 2) || (mark[k + 1].my - w / 2 < pT[i][j].y && pT[i][j].y < mark[k].my + w / 2)) {
               if (d < w / 2) {
                 if (0 <= k && k < markNum / 3 - 1) {
-                  // 線分が垂直の場合
                   if (mark[k].mx == mark[k + 1].mx) {
                     pOL[i][j].pOLx = mark[k].mx;
                     pOL[i][j].pOLy = pT[i][j].y;
                   }
-
-                  // 線分が水平の場合
                   else if (mark[k].my == mark[k + 1].my) {
                     pOL[i][j].pOLx = pT[i][j].x;
                     pOL[i][j].pOLy = mark[k].my;
                   }
-
-                  //それ以外
                   else {
                     let m1, m2, l1, l2;
-                    //線分の傾き
                     m1 = (mark[k + 1].my - mark[k].my) / (mark[k + 1].mx - mark[k].mx);
-                    //線分のY切片
                     l1 = mark[k].my - (m1 * mark[k].mx);
 
-                    //垂線の傾き
                     m2 = -1 / m1;
-                    //垂線のY切片
                     l2 = pT[i][j].y - (m2 * pT[i][j].x);
 
-                    // 交点算出
                     pOL[i][j].pOLx = (l2 - l1) / (m1 - m2);
                     pOL[i][j].pOLy = (l2 * m1 - l1 * m2) / (m1 - m2);
                   }
                 }
-
                 else if (markNum / 3 <= k && k < markNum / 3 * 2 - 1) {
-                  // 線分が垂直の場合
                   if (mark[k].mx == mark[k + 1].mx) {
                     pOL[i][j].pOLx = mark[k].mx;
                     pOL[i][j].pOLy = pT[i][j].y;
                   }
-
-                  // 線分が水平の場合
                   else if (mark[k].my == mark[k + 1].my) {
                     pOL[i][j].pOLx = pT[i][j].x;
                     pOL[i][j].pOLy = mark[k].my;
                   }
-
-                  //それ以外
                   else {
                     let m1, m2, l1, l2;
-                    //線分の傾き
                     m1 = (mark[k + 1].my - mark[k].my) / (mark[k + 1].mx - mark[k].mx);
-                    //線分のY切片
                     l1 = mark[k].my - (m1 * mark[k].mx);
 
-                    //垂線の傾き
                     m2 = -1 / m1;
-                    //垂線のY切片
                     l2 = pT[i][j].y - (m2 * pT[i][j].x);
 
-                    // 交点算出
                     pOL[i][j].pOLx = (l2 - l1) / (m1 - m2);
                     pOL[i][j].pOLy = (l2 * m1 - l1 * m2) / (m1 - m2);
                   }
                 }
-
                 else if (markNum / 3 * 2 <= k && k < markNum - 1) {
-                  // 線分が垂直の場合
                   if (mark[k].mx == mark[k + 1].mx) {
                     pOL[i][j].pOLx = mark[k].mx;
                     pOL[i][j].pOLy = pT[i][j].y;
                   }
-
-                  // 線分が水平の場合
                   else if (mark[k].my == mark[k + 1].my) {
                     pOL[i][j].pOLx = pT[i][j].x;
                     pOL[i][j].pOLy = mark[k].my;
                   }
-
-                  //それ以外
                   else {
                     let m1, m2, l1, l2;
-                    //線分の傾き
                     m1 = (mark[k + 1].my - mark[k].my) / (mark[k + 1].mx - mark[k].mx);
-                    //線分のY切片
                     l1 = mark[k].my - (m1 * mark[k].mx);
 
-                    //垂線の傾き
                     m2 = -1 / m1;
-                    //垂線のY切片
                     l2 = pT[i][j].y - (m2 * pT[i][j].x);
-
-                    // 交点算出
+                    
                     pOL[i][j].pOLx = (l2 - l1) / (m1 - m2);
                     pOL[i][j].pOLy = (l2 * m1 - l1 * m2) / (m1 - m2);
                   }
@@ -484,6 +462,15 @@ function draw() {
         }
       }
     }
+  }
+  if(recordingChanger == true){
+    if(isRecording == true){
+      stopRecording();
+    }else{
+      startRecording();
+    }
+    isRecording = !isRecording;
+    recordingChanger = !recordingChanger;
   }
 }
 
@@ -574,6 +561,10 @@ function AnimationButton() {
   markColorChanger = false;
   isMove = true;
   stopTime = 0;
+}
+
+function RecordingButton(){
+  recordingChanger = true;
 }
 
 class pointPosition {
